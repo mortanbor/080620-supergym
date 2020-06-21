89e39ab83e4a8bde337d65c4848a34924726e72f
mortanbor <42118390+mortanbor@users.noreply.github.com>
2020-06-22 01:21:06 +0300

    Исправление мелких ошибок. Разметка для слайдера

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,38 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+// var pageHeader = document.querySelector('.page-header');
+// var headerToggle = document.querySelector('.page-header__toggle');
+//
+// pageHeader.classList.remove('page-header--nojs');
+//
+// headerToggle.addEventListener('click', function () {
+//   if (pageHeader.classList.contains('page-header--closed')) {
+//     pageHeader.classList.remove('page-header--closed');
+//     pageHeader.classList.add('page-header--opened');
+//   } else {
+//     pageHeader.classList.add('page-header--closed');
+//     pageHeader.classList.remove('page-header--opened');
+//   }
+// });
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+// Slider trainer
+// var sliderTrainer = new Swiper('.slider--trainer', {
+//   slidesPerView: 1,
+//   slidesPerGroup: 1,
+//   navigation: {
+//     nextEl: '.trainer-button-next',
+//     prevEl: '.trainer-button-prev'
+//   },
+//   breakpoints: {
+//     768: {
+//       slidesPerView: 2,
+//       slidesPerGroup: 2,
+//       spaceBetween: 20
+//     },
+//     1100: {
+//       slidesPerView: 4,
+//       slidesPerGroup: 4,
+//       spaceBetween: 40
+//     }
+//   }
+// });

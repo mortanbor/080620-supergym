a50b0bb6da707a9c0e65dca1ff154a8b83c4bdfa
mortanbor <42118390+mortanbor@users.noreply.github.com>
2020-06-22 16:12:38 +0300

    Slider

--- build/js/script.js
+++ build/js/script.js
@@ -17,22 +17,44 @@
 
 // Slider trainer
 // var sliderTrainer = new Swiper('.slider--trainer', {
-//   slidesPerView: 1,
-//   slidesPerGroup: 1,
-//   navigation: {
-//     nextEl: '.trainer-button-next',
-//     prevEl: '.trainer-button-prev'
-//   },
-//   breakpoints: {
-//     768: {
-//       slidesPerView: 2,
-//       slidesPerGroup: 2,
-//       spaceBetween: 20
-//     },
-//     1100: {
-//       slidesPerView: 4,
-//       slidesPerGroup: 4,
-//       spaceBetween: 40
-//     }
-//   }
-// });
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  navigation: {
+    nextEl: '.trainer-button-next',
+    prevEl: '.trainer-button-prev'
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 20
+    },
+    1100: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      spaceBetween: 40
+    }
+  }
+});
+
+// Slider trainer
+var mySwiper = new Swiper ('.swiper-container', {
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 20
+    },
+    1100: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      spaceBetween: 40
+    }
+  }
+});

e430819432dfa3f66f6df872c41811a7f9c5d401
mortanbor <42118390+mortanbor@users.noreply.github.com>
2020-07-07 17:39:47 +0300

    Убрала двойной фон

--- build/js/script.js
+++ build/js/script.js
@@ -38,3 +38,42 @@ var sliderReview = new Swiper('.slider--reviews', {
 IMask(document.querySelector('#phonefield'), {
   mask: '+{7}(000)000-00-00',
 });
+
+//var
+var cardblockLink= document.querySelectorAll('.card-block__link');
+var choicecardLists = document.querySelectorAll('.choice-card__list');
+var anchors = choicecardLists.querySelectorAll('a[href^="#"]');
+
+// Polyfill
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = Array.prototype.forEach;
+}
+
+// Anchors
+anchors.forEach(function (anchor) {
+  anchor.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    var href = anchor.getAttribute('href');
+    var target = document.querySelector(href);
+    if (target) {
+      target.scrollIntoView({behavior: 'smooth'});
+    }
+  });
+});
+
+// Add class "active" on subscription__btn and subscription__list
+cardblockLink.forEach(function (cardblockLink, index) {
+  cardblockLink.addEventListener('click', function () {
+    // Tabs button
+    cardblockLink.forEach(function (a) {
+      btn.classList.remove('card-block__link--active');
+    });
+    cardblockLink.classList.add('card-block__link--active');
+
+    // Tabs content
+    choicecardLists.forEach(function (list) {
+      list.classList.remove('choice-card__list--active');
+    });
+    choicecardLists[index].classList.add('choice-card__list--active');
+  });
+});
